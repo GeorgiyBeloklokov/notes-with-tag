b40e5a88dc076a4ed8b2dc82a76be69b6fe53eaf
georg <georgiy.reus@gmail.com>
2021-09-05 23:04:34 +0300

    editor with tag - control point 1 - all work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import ToDoForm from "./todoform";
+import ToDo from "./todo";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([])
+    const addTask = (userInput) => {
+     if(userInput){
+    const newItem = {
+        id: Math.random().toString(36).substr(2,9),
+        task: userInput,
+        complete: false
+    }
+    setTodos([...todos,newItem])
+}
+    }
+    const removeTask = () => {
+
+    }
+    const handleToggle = () => {
+
+    }
+    return (
+        <div className="App">
+            <header>
+                <h1>Список задач: {todos.length}</h1>
+            </header>
+            <ToDoForm addTask={addTask} />
+            {todos.map((todo) => {
+                return (
+                    <ToDo
+                        todo={todo}
+                        key={todo.id}
+                        toggleTask={handleToggle}
+                        removeTask={removeTask}
+                    />
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
