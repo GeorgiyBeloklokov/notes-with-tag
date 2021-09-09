64976f31ff1a9623fbe3ebb74a2affd958508ed1
georg <georgiy.reus@gmail.com>
2021-09-09 12:12:15 +0300

    editor with tag - control point 3 - all work, edit window, introducion new bejavior

--- src/App.js
+++ src/App.js
@@ -8,30 +8,55 @@ function App() {
     const [todos, setTodos] = useState([])
 
     const addTask = (userInput) => {
-     if(userInput){
-    const newItem = {
-        id: Math.random().toString(36).substr(2,9),
-        task: userInput,
-        complete: false
-    }
-    setTodos([...todos,newItem])
-}
+        if (userInput) {
+            const newItem = {
+                id: Math.random().toString(36).substr(2, 9),
+                task: userInput,
+                complete: false
+            }
+            setTodos([...todos, newItem])
+        }
     }
     const removeTask = (id) => {
-setTodos([...todos.filter((todo) =>todo.id !== id)])
+        setTodos([...todos.filter((todo) => todo.id !== id)])
     }
-    const handleToggle = (id) => { setTodos([...todos.map((todo) =>
-        todo.id ===id ? {...todo, complete: !todo.complete} : {...todo}
 
-    )])
+    const removeTaskOld = (todo,id,userInput) => {
+        let val = todos
+        val.splice(0,9,userInput)
+        setTodos([...todos, todos] )
+    }
+
+
+    const handleToggle = (id) => {
+        setTodos([...todos.map((todo) =>
+            todo.id === id ? {...todo, complete: !todo.complete} : {...todo}
+        )])
+    }
+
+    const editTask = (userInput) => {
+        if (userInput) {
+            const newItem = {
+                id: Math.random().toString(36).substr(2, 9),
+                task: userInput,
+                complete: false
+            }
+            setTodos([ newItem])
+        }
 
     }
+
+
+
+
+
+
     return (
         <div className="App">
             <header>
                 <h1>Список задач: {todos.length}</h1>
             </header>
-            <ToDoForm addTask={addTask} />
+            <ToDoForm addTask={addTask}/>
             {todos.map((todo) => {
                 return (
                     <ToDo
@@ -39,6 +64,9 @@ setTodos([...todos.filter((todo) =>todo.id !== id)])
                         key={todo.id}
                         toggleTask={handleToggle}
                         removeTask={removeTask}
+                        editTask={editTask}
+                        removeTaskOld={removeTaskOld}
+
                     />
                 )
             })}
--- src/todo.js
+++ src/todo.js
@@ -1,4 +1,7 @@
-function ToDo({todo, toggleTask, removeTask}) {
+
+
+/*
+function ToDo({todo,toggleTask,removeTask,editTask,userInput}) {
 
     return (
         <div key={todo.id} className="item-todo">
@@ -8,6 +11,63 @@ function ToDo({todo, toggleTask, removeTask}) {
             <div className="item-delete" onClick={() => removeTask(todo.id)}>
                 X
             </div>
+            <div className="item-delete edit" onClick={() => editTask(userInput)}>
+            EDIT
+            </div>
+        </div>
+    )
+}
+
+export default ToDo;
+*/
+
+
+import {useState} from "react";
+
+const ToDo = ({editTask, removeTaskOld, todo, toggleTask, removeTask}) => {
+
+    let [editMode, setEditMode] = useState(false);
+
+    let [userInput, setUserInput] = useState('');
+
+
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode =() => {
+        setEditMode(false);
+        editTask(userInput)
+        setUserInput("")
+        /*removeTaskOld(todo.id,userInput)*/
+    }
+
+    const OnTaskChange = (e) => {
+        setUserInput(e.currentTarget.value);
+
+    }
+
+    return (
+        <div  >
+            {!editMode &&
+            <div key={todo.id} className="item-todo">
+                <div className={todo.complete ? "item-text strike" : "item-text"} onClick={() => toggleTask(todo.id)}>
+                    {todo.task}
+                </div>
+                <div className="item-delete" onClick={() => removeTask(todo.id)}>
+                    X
+                </div>
+                <div className="item-delete edit" onClick = {activateEditMode}>
+                    EDIT
+                </div>
+            </div>
+            }
+            {editMode &&
+            <div>
+                <input  onChange={OnTaskChange} onBlur={deactivateEditMode} autoFocus={true} value={userInput} />
+            </div>
+            }
         </div>
     )
 }
